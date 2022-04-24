d2d1781a101d154ddaaa78bbd6d23be7558de25b
ckodhiambo <ckodhiambo1@gmail.com>
2022-04-24 21:05:31 +0300

    'final'

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,12 @@
+import { atom } from "recoil";
+
+
+export const playlistState = atom ({
+    key:"playlistState",
+    default: null,
+})
+
+export const playlistIdState = atom({
+    key:"playlistIdState",
+    default:"07GULg2e1pF3Po8Lvd910T"
+})
---
+++ atoms/songAtom.js
@@ -0,0 +1,12 @@
+import { atom } from "recoil";
+
+export const currentTrackIdState = atom({
+    key:"currentTrackIdState",
+    //Unique ID (with respect to other atoms/selectors)
+    default: null,
+});
+
+export const isPlayingState = atom({
+    key: "isPlayingState",
+    default: false,
+})
